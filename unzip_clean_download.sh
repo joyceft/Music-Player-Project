63620bb7984c3601c1d2f6bed283da5676bb6cf8
Tianyi Fang <30835410+joyceft@users.noreply.github.com>
2018-02-18 16:53:11 -0500

    Add files via upload

---
+++ unzip_clean_download.sh
@@ -0,0 +1,49 @@
+# unzip uid
+
+#cp ../Music/3_1.uids.gz ../Music/all_uid.txt.gz
+
+#gunzip ../Music/all_uid.txt.gz
+
+
+# unzip download log
+
+for f in ../Music/*_down.log.tar.gz
+
+do
+
+ echo "Processing $f"
+
+ tar -xvzf $f 
+
+done
+
+
+
+mv *_search.log ../Music/download/
+
+
+
+cp ../Music/*_down.log.gz ../Music/download/ 
+
+gunzip ../Music/search/*.gz
+
+
+
+# append file_name to each row (will be used for date)
+cd ../Music/download/
+
+for f in *.log
+
+do
+
+ echo "Processing $f"
+
+ awk -v var="$f" '{print $0,"\t",var}' $f > ${f}.fn
+
+done
+
+
+
+# cat all log with filename to one file
+
+cat ../Music/download/*.log.fn > ../Music/all_download.log.fn
---
+++ unzip_clean_play.sh
@@ -0,0 +1,49 @@
+# unzip uid
+
+cp ../Music/3_1.uids.gz ../Music/all_uid.txt.gz
+
+gunzip ../Music/all_uid.txt.gz
+
+
+# unzip play log
+
+for f in ../Music/*_play.log.tar.gz
+
+do
+
+ echo "Processing $f"
+
+ tar -xvzf $f 
+
+done
+
+
+
+mv *_play.log ../Music/play/
+
+
+
+cp ../Music/*_play.log.gz ../Music/play/ 
+
+gunzip ../Music/play/*.gz
+
+
+
+# append file_name to each row (will be used for date)
+cd ../Music/play/
+
+for f in *.log
+
+do
+
+ echo "Processing $f"
+
+ awk -v var="$f" '{print $0,"\t",var}' $f > ${f}.fn
+
+done
+
+
+
+# cat all log with filename to one file
+
+cat ../Music/play/*.log.fn > ../Music/all_play.log.fn
