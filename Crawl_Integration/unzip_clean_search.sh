05d0c8663aba8f42ee220c7e0aba13dee6681ad4
Tianyi Fang <30835410+joyceft@users.noreply.github.com>
2018-03-05 13:08:57 -0500

    Add files via upload

---
+++ Crawl_Integration/unzip_clean_search.sh
@@ -0,0 +1,49 @@
+# unzip uid
+
+#cp ../Music/3_1.uids.gz ../Music/all_uid.txt.gz
+
+#gunzip ../Music/all_uid.txt.gz
+
+
+# unzip search log
+
+for f in ../Music/*_search.log.tar.gz
+
+do
+
+ echo "Processing $f"
+
+ tar -xvzf $f 
+
+done
+
+
+
+mv *_search.log ../Music/search/
+
+
+
+cp ../Music/*_search.log.gz ../Music/search/ 
+
+gunzip ../Music/search/*.gz
+
+
+
+# append file_name to each row (will be used for date)
+cd ../Music/search/
+
+for f in *.log
+
+do
+
+ echo "Processing $f"
+
+ awk -v var="$f" '{print $0,"\t",var}' $f > ${f}.fn
+
+done
+
+
+
+# cat all log with filename to one file
+
+cat ../Music/search/*.log.fn > ../MusicFile/all_search.log.fn
