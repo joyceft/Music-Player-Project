c53e49cea58bf0632062d0b035491a9ecd7a3653
Tianyi Fang <30835410+joyceft@users.noreply.github.com>
2018-03-08 12:26:09 -0500

    Update unzip_clean_download.sh

--- Crawl_Integration/unzip_clean_download.sh
+++ Crawl_Integration/unzip_clean_download.sh
@@ -4,7 +4,7 @@
 for f in ../Music/*_down.log.tar.gz
 do
  echo "Processing $f"
- tar -xvzf $f -O > "${f%.tar.gz}"
+ tar -xvzf $f -O > "${f%.tar.gz}" #only take filename before .tar.gz, which is '20170301_down', the extension is .log
 done
 
 #move all unzipped down.log into Music/download/ file 
@@ -13,12 +13,12 @@ mv *_down.log ../Music/download/
 # go to the download file workplace, then append file_name to each row (will be used for date)
 cd /cygdrive/e/projects/Music/download/
 
-for f in *.log
+for f in ../download/*_down.log
 do
  echo "Processing $f"
  awk -v var="$f" '{print $0,"\t",var}' $f > ${f}.fn
 done
 
 # cat all log with filename to one file
-
-cat ../Music/download/*.log.fn > ../d/MusicFile/all_download.log.fn
+#no need to create all_download_1.log.fn before, if it is not exist, it will be generated once run the file
+cat ../download/*log.fn > ../download/all_download_1.log.fn
